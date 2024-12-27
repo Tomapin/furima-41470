4989d4d150ac6fce2ff2e64aa40d076c24c4d069
Tomapin <astr1999.22.1155@gmail.com>
2024-12-27 17:18:23 +0900

    コードレビューの指摘修正①

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -17,19 +17,8 @@ class ItemsController < ApplicationController
   end
 
   private
-
-
-  def basic_auth
-    authenticate_or_request_with_http_basic do |username, password|
-      username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
-    end
-  end
   
   def index
-  end
-
-  def message_params
-    params.require(:item).permit(:name, :image, :description, :category_id, :status_id, :shipping_fee_id, :prefecture_id, :delivery_time_id, :price).merge(user_id: current_user.id)
   end
 
   def item_params
