ba54aa663b020b410d22f26f434058ad97c211db
Tomapin <astr1999.22.1155@gmail.com>
2024-12-30 20:46:23 +0900

    購入機能①

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,40 @@
 class OrdersController < ApplicationController
+  before_action :set_item, only: [:index,:create]
+
   def index
-    @item = Item.find(params[:item_id])
+    gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+    @item_order=ItemOrder.new
  end
+
+  def create
+    @item_order=ItemOrder.new(order_params)    
+    if @item_order.valid?
+     pay_item
+      @item_order.save
+      return redirect_to root_path
+    else
+      gon.public_key = ENV["PAYJP_PUBLIC_KEY"]
+      render 'index', status: :unprocessable_entity
+    end    
+  end
+
+  private
+
+  def order_params
+    params.require(:item_order).permit(:postcode,:prefecture_id,:city,:block,:building,:phone_number).merge(user_id: current_user.id, item_id:params[:item_id],token:params[:token])
+  end
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
+  end
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_30_065843) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_30_111918) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -55,12 +55,21 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_30_065843) do
   end
 
   create_table "orders", charset: "utf8mb3", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "item_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-        t.index ["item_id"], name: "index_orders_on_item_id"
-    t.index ["user_id"], name: "index_orders_on_user_id"
+  end
+
+  create_table "payments", charset: "utf8mb3", force: :cascade do |t|
+    t.bigint "order_id", null: false
+    t.string "postcode", default: "", null: false
+    t.integer "prefecture_id", null: false
+    t.string "city", default: "", null: false
+    t.string "block", default: "", null: false
+    t.string "building"
+    t.string "phone_number", default: "", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["order_id"], name: "index_payments_on_order_id"
   end
 
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
@@ -84,6 +93,5 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_30_065843) do
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "items", "users"
-  add_foreign_key "orders", "items"
-  add_foreign_key "orders", "users"
+  add_foreign_key "payments", "orders"
 end
